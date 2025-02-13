92b7d1d061f141b52942e358e3c126abe888f9d7
Tevaalgorithms <Tevaalgorithms@gmail.com>
2025-02-12 23:24:49 -0700

    Update API

--- API/CodePluse.API/CodePluse.API/Controllers/CategoriesController.cs
+++ API/CodePluse.API/CodePluse.API/Controllers/CategoriesController.cs
@@ -1,8 +1,6 @@
-﻿using CodePluse.API.Data;
-using CodePluse.API.Models.Domain;
+﻿using CodePluse.API.Models.Domain;
 using CodePluse.API.Models.DTO;
 using CodePluse.API.Repositories.Interface;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CodePluse.API.Controllers
@@ -19,7 +17,7 @@ namespace CodePluse.API.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Category(CreateCategoryRequestDto request)
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
         {
             // Map DTO to Domain Model
             var category = new Category
@@ -40,5 +38,100 @@ namespace CodePluse.API.Controllers
 
             return Ok(response);
         }
+        // GET: https://localhost:7075/api/Categories
+        [HttpGet]
+        public async Task<IActionResult> GetCategories()
+        {
+           var categories = await categoryRepository.GetAllAsync();
+            // Never expose the Domain models
+            // Map Domain model to DTO
+            var response = new List<CategoryDto>();
+            foreach (var category in categories) {
+                response.Add(new CategoryDto
+                {
+                    id=category.id,
+                    Name = category.Name,
+                    UrlHandle = category.UrlHandle
+                });
+            }
+            return Ok(response);    
+        }
+
+        // GET: https://localhost:7075/api/Categories/{id}
+        [HttpGet]
+        [Route("{categoryId:Guid}")]
+        public async Task<IActionResult> GetCategoryById([FromRoute] Guid categoryId)
+        {
+            var existingcategory = await categoryRepository.GetByIdAsync(categoryId);
+
+            if(existingcategory == null)
+            {
+                return NotFound();
+            }
+
+            // Map Domain model to DTO
+            var response = new CategoryDto
+            {
+                id = existingcategory.id,
+                Name = existingcategory.Name,
+                UrlHandle = existingcategory.UrlHandle
+            };
+
+            return Ok(response);
+        }
+
+        //PUT: https://localhost:7075/api/Categories/{id}
+        [HttpPut]
+        [Route("{categoryId:Guid}")]
+        public async Task<IActionResult> UpdateCategory([FromRoute] Guid categoryId, UpdateCategoryRequestDto request)
+        {
+            // Convery the request/(AKA DTO) to Domain Model
+            var category = new Category
+            {
+                id = categoryId,
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            category = await categoryRepository.UpdateAsync(category);
+
+            if(category == null)
+            {
+                return NotFound();
+            }
+
+            // Map Domain model to DTO
+            var response = new CategoryDto
+            {
+                id = category.id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            return Ok(response);
+
+        }
+
+        // DELETE: https://localhost:7075/api/Categories/{id}
+        [HttpDelete]
+        [Route("{categoryId:Guid}")]
+        public async Task<IActionResult> DeleteCategory([FromRoute] Guid categoryId)
+        {
+            var category = await categoryRepository.DeleteAsync(categoryId);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            //Convert Domain model to DTO
+            var response = new CategoryDto
+            {
+                id = category.id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+            return Ok(response);
+        }
+
+
     }
 }
--- API/CodePluse.API/CodePluse.API/Repositories/Implementation/CategoryRepository.cs
+++ API/CodePluse.API/CodePluse.API/Repositories/Implementation/CategoryRepository.cs
@@ -1,7 +1,7 @@
 ﻿using CodePluse.API.Data;
 using CodePluse.API.Models.Domain;
 using CodePluse.API.Repositories.Interface;
-
+using Microsoft.EntityFrameworkCore;
 
 namespace CodePluse.API.Repositories.Implementation
 {
@@ -18,6 +18,41 @@ namespace CodePluse.API.Repositories.Implementation
             await dBContext.Categories.AddAsync(category);
             await dBContext.SaveChangesAsync();
             return category;
+        }       
+
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            return await dBContext.Categories.ToListAsync();            
+        }
+
+        public async Task<Category?> GetByIdAsync(Guid id)
+        {
+            return await dBContext.Categories.FirstOrDefaultAsync(x => x.id == id);
+        }
+
+        public async Task<Category?> UpdateAsync(Category category)
+        {
+            var existingCategory = await dBContext.Categories.FirstOrDefaultAsync(x => x.id == category.id);
+
+            if(existingCategory != null)
+            {
+                dBContext.Entry(existingCategory).CurrentValues.SetValues(category);
+                await dBContext.SaveChangesAsync();
+                return category;
+            }
+            return null;
+        }
+
+        public async Task<Category?> DeleteAsync(Guid id)
+        {
+            var existingCategory = await dBContext.Categories.FirstOrDefaultAsync(x => x.id == id);
+
+            if (existingCategory != null) {
+                dBContext.Categories.Remove(existingCategory);
+                dBContext.SaveChangesAsync();
+                return existingCategory;
+            }
+            return null;
         }
     }
 }
